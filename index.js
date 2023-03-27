5428541b3da24c639e27f9cb1260a1e85fe657ad
IslamelGebaly <islamalgebaly11@gmail.com>
2023-03-27 22:00:43 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title, author, publisher, ISBN){
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+    this.ISBN = ISBN;
+}
+
+function addBookToLibrary(){
+    
+}
+
+myLibrary;
