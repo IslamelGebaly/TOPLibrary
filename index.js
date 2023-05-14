8a556e76710b19b943fb63daac46f7ca53d698c2
IslamelGebaly <islamalgebaly11@gmail.com>
2023-05-14 22:37:42 +0200

    add-book functionality added

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ function Book(title, author, publisher, ISBN){
     this.ISBN = ISBN;
 }
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(title, author, publisher, ISBN){
+   const newBook = new Book(title, author, publisher, ISBN);
+   myLibrary.push(newBook);  
 }
 
-myLibrary;
+function main(){
+    const bookForm = document.getElementById("book-form");
+    bookForm.addEventListener("submit", e => {
+        e.preventDefault();
+        const bookData = {};
+        console.log("adding book....");
+        Object.keys(bookForm.elements).forEach(key => {
+            let element = bookForm.elements[key];
+            if(element.type != "submit"){
+                bookData[element.name] = element.value;
+            }
+        });
+        console.log(bookData);
+        addBookToLibrary(bookData["title"], bookData["author"], bookData["publisher"], bookData["ISBN"]);
+    })
+}
+
+main();
