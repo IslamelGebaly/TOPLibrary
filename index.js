af5d68151679f00bb3aaaf7af6a59456d873545e
IslamelGebaly <islamalgebaly11@gmail.com>
2023-05-16 20:40:38 +0200

    added style sheet

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ function Book(title, author, publisher, ISBN, id){
 }
 
 function displayBook(book){
-    const item = document.createElement("div");
+    const item = document.createElement("tr");
+    
     item.setAttribute("id", book.id);
-    item.appendChild(document.createElement("label").appendChild(document.createTextNode(`Title : ${book.title}`)));
-    item.appendChild(document.createElement("label").appendChild(document.createTextNode(`Author : ${book.author}`)));
-    item.appendChild(document.createElement("label").appendChild(document.createTextNode(`Publisher : ${book.publisher}`)));
-    item.appendChild(document.createElement("label").appendChild(document.createTextNode(`ISBN : ${book.ISBN}`)));
+    const title = document.createElement("td");
+    title.appendChild(document.createTextNode(`${book.title}`));
+    item.appendChild(title);
 
+    const author = document.createElement("td");
+    author.appendChild(document.createTextNode(`${book.author}`));
+    item.appendChild(author);
+
+    const publisher = document.createElement("td");
+    publisher.appendChild(document.createTextNode(`${book.publisher}`));
+    item.appendChild(publisher);
+
+    const isbn = document.createElement("td");
+    isbn.appendChild(document.createTextNode(`${book.ISBN}`));
+    item.appendChild(isbn);
+    
     const rmButton = document.createElement("button");
-    rmButton.appendChild(document.createTextNode("remove"));
+    rmButton.appendChild(document.createTextNode("-"));
     rmButton.onclick = () => {
         let index = myLibrary.indexOf(myLibrary.find(element => element.id === Number.parseInt(item.getAttribute("id"))));
         myLibrary.splice(index, 1);
@@ -35,9 +47,9 @@ function displayBook(book){
             readButton.setAttribute("class", "not-read");
     };
 
-    item.appendChild(rmButton);
-    item.appendChild(readButton);
-
+    
+    item.appendChild((document.createElement("td")).appendChild(readButton));
+    item.appendChild((document.createElement("td")).appendChild(rmButton));
     bookContainer.appendChild(item);
 }
 
@@ -47,14 +59,44 @@ function addBookToLibrary(title, author, publisher, ISBN){
 }
 
 function loadBooks(){
+
     bookContainer.replaceChildren();
+    const headerRow = document.createElement("tr");
+
+    const title = document.createElement("th");
+    title.appendChild(document.createTextNode("Title"));
+    headerRow.append(title);
+
+    const author = document.createElement("th");
+    author.appendChild(document.createTextNode("Author"));
+    headerRow.append(author);
+
+    const publisher = document.createElement("th");
+    publisher.appendChild(document.createTextNode("Publisher"));
+    headerRow.append(publisher);
+
+    const isbn = document.createElement("th");
+    isbn.appendChild(document.createTextNode("ISBN"));
+    headerRow.append(isbn);
+
+    const  read = document.createElement("th");
+    read.appendChild(document.createTextNode("Read"));
+    headerRow.append(read);
+
+    const remove = document.createElement("th");
+    remove.appendChild(document.createTextNode("Remove"));
+    headerRow.append(remove);
+
+    
+    bookContainer.appendChild(headerRow);
+    
     for(let book of myLibrary){
         displayBook(book);
     }
 }
 
 function main(){
-
+    loadBooks()
     const bookForm = document.getElementById("book-form");
     bookForm.addEventListener("submit", e => {
         e.preventDefault();
